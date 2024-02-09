d14c8a3ccde6d52c55cad1b7a8b6f4ce47974b30
Luke Lento <luke.lento@student.allenisd.org>
2024-02-09 14:10:48 -0600

    Added functionality for building a 'Levels' screen

--- html/prototypeSite/home.js
+++ html/prototypeSite/home.js
@@ -1,8 +1,24 @@
+const fs = require('fs');
+const root = '../..';
+const levelsDir =  root.concat('/IgisLevelEditor/Levels');
+const assets = root.concat('/assets')
+const textures = assets.concat('/textures/ui/titleScreen');
+
+var scene
+
 
 const canvas = document.getElementById("renderCanvas"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
-function createScene(){
+function fetchLevels(){
+    var levels = [];
+
+    levels = fs.readdir(levelsDir);
+    
+    return levels;
+}
+
+function createScene(level){
     // Creates a basic Babylon Scene object
     const scene = new BABYLON.Scene(engine);  // Creates and positions an Arc Rotating camera
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 10), scene);  // Targets the camera to scene origin
@@ -13,12 +29,30 @@ function createScene(){
     const box = BABYLON.MeshBuilder.CreateBox("box", { size: 2}, scene);       // Move the sphere upward 1/2 its height
     box.position.y = 1;       // Built-in 'ground' shape.
     const ground = BABYLON.MeshBuilder.CreateGround("ground",     { width: 6, height: 6 }, scene);
+    const tex = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI");
+
+    if (level == "levels"){
+	var bg = new BABYLON.Image(textures.concat('.bg_1.png'))
+	bg.width = canvas.width
+	bg.height = canvas.height
+	
+	tex.addControl(bg);
+	
+	
+    }
 
     return scene;
 }
 
+
 function play(){
-    var scene = createScene();
+    if (scene) {
+	return;
+    }
+    scene = createScene("levels");
+
+    
+    
 
     engine.runRenderLoop(function(){
 	scene.render();	
