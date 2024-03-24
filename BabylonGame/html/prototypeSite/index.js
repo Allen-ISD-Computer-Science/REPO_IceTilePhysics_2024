6ca800a7eb4838a0e8c74bc2c8ce137b207c6225
lukelento06 <lentoluke0@gmail.com>
2024-03-24 02:21:12 -0500

    Made levels load from level select screen - Added code from levelGenerator.html to load levels - Currently only one face is being loaded, but it still works - Levels 3-8 to 3-10 do not currently exist, could rename other levels to use as placeholders for those

--- BabylonGame/html/prototypeSite/index.js
+++ BabylonGame/html/prototypeSite/index.js
@@ -8,6 +8,7 @@ const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engi
 
 var scene;
 
+
 function fetchWorld(index){
     return new Promise((resolve, reject) => {
         fetch("campaign.json")
@@ -28,7 +29,19 @@ function fetchWorld(index){
     });
 }
 
-function generateLevels(worldData){
+function trackClick(rect, lvlName, grid, worldLabelContainer){
+    rect.onPointerUpObservable.add(function(){
+        createScene(lvlName)
+        clearLevelSelect(grid, worldLabelContainer)
+    });
+}
+
+function clearLevelSelect(grid, worldLabelContainer){
+    grid.dispose()
+    worldLabelContainer.dispose()
+}
+
+function generateLevels(worldData, worldLabelContainer){
         //defining the grid
         const grid = new BABYLON.GUI.Grid();
 
@@ -59,6 +72,7 @@ function generateLevels(worldData){
     
             rect.background = color;
             rect.thickness = 0;
+            rect.cornerRadius = 10
             rect.paddingBottom = '15px';
             rect.paddingRight = '15px';
             rect.paddingTop = '15px';
@@ -66,7 +80,7 @@ function generateLevels(worldData){
             rect.width = '100px'
             rect.height = '100px'
     
-            
+            trackClick(rect, lvlName, grid, worldLabelContainer)
     
             if (column < columnLimit) {
                 grid.addColumnDefinition(100, true);
@@ -75,24 +89,186 @@ function generateLevels(worldData){
                 row++;
                 column = 0;
             }
-    
+
             grid.addControl(rect, row, column);
             column++;
         }
         return grid
 }
 
+//create cube function, loads the level
+function createCube(back, left, top, right, front, bottom) {
+    var traversableTiles = [];
+
+    //cube materials
+    var grass = new BABYLON.StandardMaterial("greenMaterial", scene);
+    grass.diffuseColor = new BABYLON.Color3(0, 1, 0); // RGB for green
+
+    var red = new BABYLON.StandardMaterial("redMaterial", scene);
+    red.diffuseColor = new BABYLON.Color3(1, 0, 0); // RGB for green
+
+    var blue = new BABYLON.StandardMaterial("blueMaterial", scene);
+    blue.diffuseColor = new BABYLON.Color3(0, 0, 1);
+
+    var yellow = new BABYLON.StandardMaterial("yellowMaterial", scene);
+    yellow.diffuseColor = new BABYLON.Color3(1, 1, 0);
+
+    var purple = new BABYLON.StandardMaterial("purpleMaterial", scene);
+    purple.diffuseColor = new BABYLON.Color3(1, 0, 1);
+
+    var teal = new BABYLON.StandardMaterial("tealMaterial", scene);
+    teal.diffuseColor = new BABYLON.Color3(0, 1, 1);
+
+    var black = new BABYLON.StandardMaterial("blackMaterial", scene);
+    black.diffuseColor = new BABYLON.Color3(0, 0, 0);
+
+    for (i = 0; i < top.length; i++) {
+        for (j = 0; j < top[i].length; j++) {
+            var tileValue = top[i][j]
+            var cube = BABYLON.MeshBuilder.CreateBox("topTile", { height: 1, width: 1, depth: 1 }, scene);
+            cube.updatable = true;
+            cube.position.x = i;
+            cube.position.y = 0;
+            cube.position.z = -j + top[i].length - 1;
+            cube.test = true
+
+            if (tileValue == 0) {
+                cube.position.y = cube.position.y + .4
+
+                cube.name = "traversableTop";
+                traversableTiles.push(cube);
+            }
+        }
+    }
+    for (i = 0; i < left.length; i++) {
+        for (j = 0; j < left[i].length; j++) {
+            var tileValue = left[i][j]
+            var cube = BABYLON.MeshBuilder.CreateBox("leftTile")
+            cube.updatable = true;
+
+            cube.position.x = 0;
+            cube.position.y = -(left.length - 1 - i);
+            cube.position.z = left[i].length - 1 - j;
+            cube.test = true
+
+            if (tileValue == 0) {
+                cube.position.x = cube.position.x - .4
+
+                cube.name = "traversableLeft";
+                traversableTiles.push(cube);
+            }
+        }
+    }
+    for (i = 0; i < back.length; i++) {
+        for (j = 0; j < back[i].length; j++) {
+            var tileValue = back[i][j]
+            var cube = BABYLON.MeshBuilder.CreateBox("backTile")
+            cube.updatable = true;
+
+            cube.position.x = i;
+            cube.position.y = j - back[i].length + 1;
+            cube.position.z = back[i].length - 1;
+            cube.test = true
+            if (tileValue == 0) {
+                cube.position.z = cube.position.z + .4
+
+                cube.name = "traversableBack"
+                traversableTiles.push(cube);
+            }
+        }
+    }
+    for (i = 0; i < right.length; i++) {
+        for (j = 0; j < right[i].length; j++) {
+            var tileValue = right[i][j]
+            var cube = BABYLON.MeshBuilder.CreateBox("rightTile") //right
+            cube.updatable = true;
+
+            cube.position.x = right[i].length - 1;
+            cube.position.y = -i;
+            cube.position.z = right[i].length - 1 - j;
+            cube.test = true
+            if (tileValue == 0) {
+                cube.position.x = cube.position.x + .4
+                cube.name = "traversableRight"
+                traversableTiles.push(cube);
+            }
+        }
+    }
+    for (i = 0; i < front.length; i++) {
+        for (j = 0; j < front[i].length; j++) {
+            var tileValue = front[i][j]
+            var cube = BABYLON.MeshBuilder.CreateBox("frontTile")
+            cube.updatable = true;
+
+            cube.position.x = i;
+            cube.position.y = -j;
+            cube.position.z = 0;
+            cube.test = true
+            if (tileValue == 0) {
+                cube.position.z = cube.position.z - .4
+
+                cube.name = "traversableFront";
+                traversableTiles.push(cube);
+            }
+        }
+    }
+    for (i = 0; i < bottom.length; i++) {
+        for (j = 0; j < bottom[i].length; j++) {
+            var tileValue = bottom[i][j]
+            var cube = BABYLON.MeshBuilder.CreateBox("bottomTile")
+            cube.updatable = true;
+
+            cube.position.x = i;
+            cube.position.y = -bottom[i].length + 1;
+            cube.position.z = j;
+            cube.test = true
+            if (tileValue == 0) {
+                cube.position.y = cube.position.y - .4
+
+                cube.name = "traversableBottom";
+                traversableTiles.push(cube);
+            }
+        }
+    }
+    for (c = 0; c < traversableTiles.length; c++) {
+        if (traversableTiles[c].name == "traversableTop") {
+            traversableTiles[c].material = grass;
+        } else if (traversableTiles[c].name == "traversableLeft") {
+            traversableTiles[c].material = blue;
+        } else if (traversableTiles[c].name == "traversableBack") {
+            traversableTiles[c].material = yellow;
+        } else if (traversableTiles[c].name == "traversableRight") {
+            traversableTiles[c].material = purple;
+        } else if (traversableTiles[c].name == "traversableFront") {
+            traversableTiles[c].material = teal;
+        } else if (traversableTiles[c].name == "traversableBottom") {
+            traversableTiles[c].material = black
+        }
+    }
+
+    loop: for (z = 0; z < traversableTiles.length; z++) {
+        for (y = 0; y < traversableTiles.length; y++) {
+            if (BABYLON.Vector3.DistanceSquared(traversableTiles[y].position, traversableTiles[z].position) < 1.0) {
+                if (traversableTiles[y].name == traversableTiles[z].name) continue;
+                traversableTiles[y].material = red
+                traversableTiles[z].material = red
+            }
+        }
+    }
+}
+
 
 function createScene(level){
     // Creates a basic Babylon Scene object
     const scene = new BABYLON.Scene(engine);  // Creates and positions an Arc Rotating camera
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 10), scene);  // Targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());  // This attaches the camera to the canvas
       // Creates a light, aiming 0,1,0 - to the sky
-    const light = new BABYLON.HemisphericLight("light",                                            new BABYLON.Vector3(0, 1, 0), scene);       // Dim the light a small amount - 0 to 1
-    light.intensity = 0.7;     
-    const box = BABYLON.MeshBuilder.CreateBox("box", { size: 2}, scene);       // Move the sphere upward 1/2 its height
-    box.position.y = 1;    
+      var camera = new BABYLON.ArcRotateCamera("camera", BABYLON.Tools.ToRadians(50), BABYLON.Tools.ToRadians(65), 20, BABYLON.Vector3.Zero(), scene);
+      camera.setTarget(BABYLON.Vector3.Zero());
+      camera.attachControl(canvas, true);
+
+      var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 5, 0), scene);
+      var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, -5, 0), scene);
+      light.intensity = 0.7;
     
     const tex = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI");
 
@@ -162,10 +338,11 @@ function createScene(level){
             selectedWorld = 0
         }
 
+        levelGrid.dispose()
         fetchWorld(selectedWorld)
             .then(
                 worldData => {
-                var levelGrid = generateLevels(worldData)
+                levelGrid = generateLevels(worldData, worldLabelContainer)
                 tex.addControl(levelGrid);
             }
         )
@@ -179,10 +356,11 @@ function createScene(level){
             selectedWorld = 2
         }
 
+        levelGrid.dispose()
         fetchWorld(selectedWorld)
             .then(
                 worldData => {
-                var levelGrid = generateLevels(worldData)
+                levelGrid = generateLevels(worldData, worldLabelContainer)
                 tex.addControl(levelGrid);
             }
         )
@@ -193,10 +371,42 @@ function createScene(level){
     fetchWorld(selectedWorld)
     .then(
         worldData => {
-            var levelGrid = generateLevels(worldData)
+            levelGrid = generateLevels(worldData, worldLabelContainer)
             tex.addControl(levelGrid);
         }
     )
+    } else {
+        // Variable Initialization
+
+        var importedLevel;
+        var importedBack;
+        var importedLeft;
+        var importedTop;
+        var importedRight;
+        var importedFront;
+        var importedBottom;
+
+        var levelDirectory = '../../../IgisLevelEditor/Levels/'.concat(level, ".lvl")
+        console.log(levelDirectory)
+
+        // Load level from file
+
+        fetch(levelDirectory)
+            .then(response => response.json())
+            .then(data => {
+                importedLevel = data.face_tiles;
+
+                importedBack = importedLevel[0];
+                importedLeft = importedLevel[1];
+                importedTop = importedLevel[2];
+                importedRight = importedLevel[3];
+                importedFront = importedLevel[4];
+                importedBottom = importedLevel[5];
+
+                createCube(importedBack, importedLeft, importedTop, importedRight, importedFront, importedBottom)
+
+            })
+            .catch(error => console.error('Error loading the JSON file:', error));
     }
 
     return scene;
